15d9a7c463127368146e1a6d5fa60bcd8621160e
katiek78 <kermodekatie@gmail.com>
2023-04-17 18:45:16 +0100

    feat: adjust location explanations

--- components/LocationExplanation.js
+++ components/LocationExplanation.js
@@ -5,10 +5,10 @@ const LocationExplanation = () => {
     return(
     <details>
         <summary>What should go here?</summary>
-    <p className='location__explanation'>Copy the co-ordinates from the address bar on Google Maps or you can simply paste the whole URL! On mobile, simply copy the coordinates.</p>
-    <img className='location__image' id="location-explanation" alt="screenshot of part of a Google Street View URL with red box around co-ordinates" src={locationExplanation.src}></img>
-    <br />
-    <img className='location__image' id="location-explanation2" alt="screenshot from Google Street View app" height={200} src={locationExplanation2.src}></img>
+    <p className='location__explanation'>Simply paste the address from Google Maps. On mobile, simply copy the coordinates.</p>
+    <img className='location__image' width={'100%'} id="location-explanation" alt="screenshot of part of a Google Street View URL with red box around co-ordinates" src={locationExplanation.src}></img>
+    <p className='location__explanation'>OR</p>
+    <img className='location__image' id="location-explanation2" alt="screenshot from Google Street View app" height={150} src={locationExplanation2.src}></img>
     </details>
     );
 }
