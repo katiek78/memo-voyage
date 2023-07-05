6da7b7f5ad443f67c4301501eb4c302261c9a88f
Katie Kermode <kermodekatie@gmail.com>
2023-07-05 13:20:27 +0100

    feat: update view page to display large streetview with title

--- pages/[id]/view.js
+++ pages/[id]/view.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import EmbedStreetView from '../../components/EmbedStreetView'
 
 const fetcher = (url) =>
   fetch(url)
@@ -20,10 +21,39 @@ const ViewPoint = () => {
   if (!journey) return null
 
   const thisJourney = Array.isArray(journey) ? journey[0] : journey;
-  const point = thisJourney.points.filter(point => point._id === id)[0];  
+  //const point = thisJourney.points.filter(point => point._id === id)[0];
 
-  return <p>Nothing here yet. {point.name} </p>
-//   return <JourneyForm formId="edit-journey-form" journeyForm={journeyForm} forNewJourney={false} />
+  function getPointData(points, id) {
+    for (let i = 0; i < points.length; i++) {
+      if (points[i]._id === id) {
+        return { point: points[i], sequenceNo: i };
+      }
+    }
+    return null; 
+  }
+
+  const pointData = getPointData(thisJourney.points, id)
+  const { point, sequenceNo } = pointData;
+  
+
+  return <>
+    <div className="title-and-content">
+        <div className="title">Journey: {thisJourney.name}</div>
+        <div className="point-card point-card-large">
+            <div className="card-content">
+                    <p className="point-name">{sequenceNo + 1}. {point.name}</p>
+                    <EmbedStreetView width={1150} height={600} location={point.location} heading={point.heading || 90} pitch={point.pitch || 0} fov={point.fov || 100} />
+             </div>
+        </div>
+     </div>
+    </>
+    {/* <div className="point-card point-card-large">
+        <div className="card-content">
+            <p className="point-name">{point.name}</p>
+            <EmbedStreetView width={300} height={200} location={point.location} heading={point.heading || 90} pitch={point.pitch || 0} fov={point.fov || 100} />
+        </div>
+    </div> */}
+   
 }
 
 export default ViewPoint
